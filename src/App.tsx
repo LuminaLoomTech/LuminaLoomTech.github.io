e0e5e9eec35414ff2a3e3731f6a2de87ee657646
JackWong-kiori <waynejackzgz@gmail.com>
2025-10-12 22:05:20 +0800

    更新手機響應

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import './App.css';
-import { Routes, Route,useLocation } from 'react-router-dom';
 import Home from './pages/home/Home';
 import About from './pages/about/AboutPage';
-import Sidebar from './components/sidebar/Sidebar'
-import Footer from './components/footer/Footer'
+import Sidebar from './components/sidebar/Sidebar';
+import Footer from './components/footer/Footer';
 import ContactForm from './pages/contact/Contact';
+import { HashRouter, Routes, Route, useLocation } from "react-router-dom";
 
 function NotReady() {
   return (
-     <h1>此頁面尚未完成，敬請期待！</h1>
-  )
+    <h1>此頁面尚未完成，敬請期待！</h1>
+  );
 }
 
-function App() {
+// 包一個 component 使用 useLocation
+function AppRoutes() {
   const location = useLocation();
 
   const menuItems = [
@@ -22,24 +23,33 @@ function App() {
     { label: "聯絡", link: "/contact" },
     { label: "登入", link: "/login" }
   ];
+
   return (
     <>
-    {/* Sidebar 全站顯示 */}
-    <Sidebar item={menuItems} />
+      {/* Sidebar 全站顯示 */}
+      <Sidebar item={menuItems} />
 
-    {/* 主要內容容器，避免被固定 Footer 蓋住 */}
-    <div className="app-content">
-      <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<NotReady />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<ContactForm />} />
-      </Routes>
-    </div>
+      {/* 主要內容容器，避免被固定 Footer 蓋住 */}
+      <div className="app-content">
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<NotReady />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<ContactForm />} />
+        </Routes>
+      </div>
 
-    {/* 全站固定 Footer */}
-    <Footer />
+      {/* 全站固定 Footer */}
+      <Footer />
     </>
+  );
+}
+
+function App() {
+  return (
+    <HashRouter>
+      <AppRoutes />
+    </HashRouter>
   );
 }
 
